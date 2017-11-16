f1d3c09cc2941f1858567b3557a8ec432ebe2a98
Kevin Fox <kevin@efox.cc>
2017-11-15 17:24:12 -0800

    Add volume and more point at p7zip more directly.

--- prep.sh
+++ prep.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+
 [ ! -f CentOS-7-x86_64-DVD-1708.iso ] && curl -L 'http://mirrors.usc.edu/pub/linux/distributions/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1708.iso' -o CentOS-7-x86_64-DVD-1708.iso
 
 if [ ! -d data ]; then
 	curl -L 'https://sourceforge.net/projects/p7zip/files/p7zip/16.02/p7zip_16.02_x86_linux_bin.tar.bz2/download' -o 7zip.tar.bz2
 	tar -jxvf 7zip.tar.bz2
+	chmod +x "$DIR"/p7zip_16.02/bin/7z
 	mkdir data
 	pushd data
-	../p7zip_16.02/bin/7z x ../CentOS-7-x86_64-DVD-1708.iso
+	"$DIR"/p7zip_16.02/bin/7z x ../CentOS-7-x86_64-DVD-1708.iso
 	popd
 	chmod --recursive 755 data
 fi
