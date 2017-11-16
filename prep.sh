ac8d49412ded604999c8c22087fb643d5d7ed41a
Kevin Fox <kevin@efox.cc>
2017-11-16 06:04:26 -0800

    Try this.

--- prep.sh
+++ prep.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 echo "Working dir: `pwd`"
 
-#[ ! -f CentOS-7-x86_64-DVD-1708.iso ] && curl -L 'http://mirrors.usc.edu/pub/linux/distributions/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1708.iso' -o CentOS-7-x86_64-DVD-1708.iso
+[ ! -f CentOS-7-x86_64-DVD-1708.iso ] && curl -L 'http://mirrors.usc.edu/pub/linux/distributions/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1708.iso' -o CentOS-7-x86_64-DVD-1708.iso
 
 if [ ! -d data ]; then
 	curl -L 'https://sourceforge.net/projects/p7zip/files/p7zip/16.02/p7zip_16.02_x86_linux_bin.tar.bz2/download' -o 7zip.tar.bz2
@@ -14,7 +14,6 @@ if [ ! -d data ]; then
 	mkdir data
 	pushd data
 	ls -l "$DIR"/p7zip_16.02/bin/7z
-	ldd "$DIR"/p7zip_16.02/bin/7z
 	"$DIR"/p7zip_16.02/bin/7z x ../CentOS-7-x86_64-DVD-1708.iso
 	popd
 	chmod --recursive 755 data
